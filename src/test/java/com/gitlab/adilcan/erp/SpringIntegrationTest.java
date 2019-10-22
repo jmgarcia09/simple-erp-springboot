1bbeff5f5406ba4ebe559283b1f64602c384037e
jmgarcia09 <Breakmaster09>
2019-10-21 23:30:23 -0600

    Use of remote webdriver

--- src/test/java/com/gitlab/adilcan/erp/SpringIntegrationTest.java
+++ src/test/java/com/gitlab/adilcan/erp/SpringIntegrationTest.java
@@ -6,12 +6,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.fail;
@@ -28,9 +31,11 @@ import static org.junit.Assert.fail;
     //@Before
     public void setUp() throws Exception {
         if(driver == null){
-            System.out.println(WebDriverManager.chromedriver().getVersions());
-            WebDriverManager.chromedriver().version("77.0.3865.40").setup();
-            driver = new ChromeDriver();
+            //System.out.println(WebDriverManager.chromedriver().getVersions());
+            //WebDriverManager.chromedriver().version("77.0.3865.40").setup();
+            //driver = new ChromeDriver();
+            DesiredCapabilities capability = DesiredCapabilities.chrome();
+            driver = new RemoteWebDriver(new URL("http://192.168.99.101:4444/wd/hub"), capability);
             baseUrl = "https://www.katalon.com/";
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         }
